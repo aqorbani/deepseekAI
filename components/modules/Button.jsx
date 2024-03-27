11ca66ee82b4cff662d12fb53ca4d19d5bae8b81
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-27 16:08:36 +0330

    edit button font size

--- components/modules/Button.jsx
+++ components/modules/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ title, functionHandler }) {
   return (
     <button
       onClick={functionHandler}
-      class="inline-flex items-center justify-center h-16 px-10 py-0 text-xl font-semibold text-center text-gray-400 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-400 border-solid rounded-full cursor-pointer select-none hover:text-gray-900 hover:border-gray-900 focus:shadow-xs focus:no-underline"
+      class="inline-flex items-center justify-center px-10 py-0 text-[2.5vw] md:text-[1.5vw] font-semibold text-center text-gray-400 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-400 border-solid rounded-full cursor-pointer select-none hover:text-gray-900 hover:border-gray-900 focus:shadow-xs focus:no-underline"
     >
       <p className="m-1">{title}</p>
       <RiSendPlaneFill className="m-1" />
