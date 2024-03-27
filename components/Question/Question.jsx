f43fe5bd29946a89571a1d7160b5d4f13f684600
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-27 14:12:43 +0330

    fix animate moving with axios

--- components/Question/Question.jsx
+++ components/Question/Question.jsx
@@ -6,6 +6,7 @@ import { IoClose } from "react-icons/io5";
 import Button from "../modules/Button";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { getAxiosConfig } from "../../utils/getAxiosConfig";
 
 export default function Question({ setLoader, redirect }) {
   const router = useRouter();
@@ -35,47 +36,16 @@ export default function Question({ setLoader, redirect }) {
     }
     // ____________________________________________________
 
-    let data = JSON.stringify({
-      messages: [
-        {
-          content: text,
-          role: "user",
-        },
-      ],
-      model: "deepseek-chat",
-      frequency_penalty: 0,
-      max_tokens: 2048,
-      presence_penalty: 0,
-      stop: null,
-      stream: false,
-      temperature: 1,
-      top_p: 1,
-    });
-
-    let config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: process.env.DEEPSEEK,
-      timeout: 15000,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: "Bearer " + process.env.KEY,
-      },
-      data: data,
-    };
-
-    axios(config)
-      .then((response) => {
-        setResponse(JSON.stringify(response.data.choices[0].message.content));
-      })
-      .catch((error) => {
-        toast.info("No response is received from the server", {
-          position: "top-center",
-          hideProgressBar: true,
-          icon: false,
-        });
+    try {
+      const response = await axios(getAxiosConfig(text));
+      setResponse(JSON.stringify(response.data.choices[0].message.content));
+    } catch (error) {
+      toast.info("No response is received from the server", {
+        position: "top-center",
+        hideProgressBar: true,
+        icon: false,
       });
+    }
     setLoader(false);
     // ____________________________________________________
     // const res = await fetch(process.env.DEEPSEEK, {
---
+++ utils/getAxiosConfig.js
@@ -0,0 +1,32 @@
+export const getAxiosConfig = (text) => {
+  let data = JSON.stringify({
+    messages: [
+      {
+        content: text,
+        role: "user",
+      },
+    ],
+    model: "deepseek-chat",
+    frequency_penalty: 0,
+    max_tokens: 2048,
+    presence_penalty: 0,
+    stop: null,
+    stream: false,
+    temperature: 1,
+    top_p: 1,
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: process.env.DEEPSEEK,
+    timeout: 15000,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + process.env.KEY,
+    },
+    data: data,
+  };
+  return config;
+};
