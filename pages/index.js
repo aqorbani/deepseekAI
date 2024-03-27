773d7b9f46d9ae227d34bc36a305b90dd7ede4f8
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-27 18:20:15 +0330

    create splash screen

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Main from "../components/Main/Main";
+import Splash from "../components/Splash/Splash";
 
 export default function Home() {
-  return <Main />;
+  const [splash, setSplash] = useState(true);
+  useEffect(() => {
+    const splashDelay = setTimeout(() => {
+      setSplash(false);
+    }, 3000);
+
+    return () => clearTimeout(splashDelay);
+  });
+  return <>{splash ? <Splash /> : <Main />}</>;
 }
